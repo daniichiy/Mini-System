25a4d25bdc871974a683880ecf7fdf8be979211f
IchiyD <ichiydani@gmail.com>
2025-05-15 17:30:02 -0400

    tela inicial

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="css/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Academia Sports</title>
+</head>
+<body>
+    <header class="bg-primary text-white text-center py-3">
+        <h1>Academia Sports</h1>
+    </header>
+
+    <section class="container my-5">
+        <h2>Sobre a Academia</h2>
+        <p>A Academia Sports é o lugar perfeito para você melhorar sua saúde, alcançar seus objetivos e transformar seu estilo de vida. Oferecemos uma estrutura completa, com equipamentos modernos e aulas especializadas.</p>
+        <div style="text-align: center;">
+            <img src="https://s3u.tmimgcdn.com/u37752224/8c43b2920349f1e0730386cc9f245316.gif" alt="Logo da academia" class="img-fluid rounded shadow" style="width: 600px; height: 400px;">
+        </div>
+    </section>
+
+    <div class="text-center">
+        <a href="#" class="btn btn-success btn-lg">Se inscreva</a>
+    </div>
+
+     <footer class="bg-dark text-white text-center py-3 mt-5">
+        <p>&copy; 2025 Academia Sports - by Daniele.</p>
+        <p>Contato: contato@academiasports.com.br | Telefone: (67) 1234-5678</p>
+    </footer>
+</body>
+</html>
